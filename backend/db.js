3a719fab4add4aa59ca2594e12813ec64981d4ae
Ritwik <ritwikhero@gmail.com>
2024-03-14 22:31:59 +0530

    MongoDB started

---
+++ backend/db.js
@@ -0,0 +1,17 @@
+/*
+    title : String.
+    description : String,
+    Completed : boolean
+*/
+
+const mongoose = require("mongoose");
+
+// mongodb+srv://ritwiksingh91099:Ritwik%409@cluster0.abno3ce.mongodb.net/
+mongoose.connect("mongodb+srv://ritwiksingh91099:Ritwik%409@cluster0.abno3ce.mongodb.net/todoApp");
+const toDoSchema = mongoose.Schema({
+    title : String,
+    description : String,
+    completed : Boolean,
+});
+
+const todo = mongoose.model("todos",toDoSchema);
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 //basic express boiler plate code with APIs and middlewares
 const express = require('express');
+const {ceateTodo, updateTodo} = require("./types");
 const app = express();
 
 app.use(express.json())
@@ -13,12 +14,29 @@ app.use(express.json())
  */
 app.post("/todo", function(req,res){
     const cratePayLoad = req.body;
-    const parsedPayLoad = ceateTodo.safe(createPayload); 
+    const parsedPayLoad = ceateTodo.safeParse(createPayload);
+    if(!parsedPayLoad.success){
+        res.status(411).json({
+            msg : "You sent wrong inputs",
+        })
+        return;
+    }
+    //put in mongoDB
+
 });
 app.get("/todos", function(req,res){
 
 });
 app.post("/completed", function(req,res){
+    const updatePayLoad = req.body;
+    const parsedPayLoad = updateTodo.safeParse(updatePayLoad);
+    if(!parsedPayLoad){
+        res.status(411).json({
+            msg : "You sent wrong inputs"
+        })
+        return;
+    }
+    //update in mongo
 
 });
 
