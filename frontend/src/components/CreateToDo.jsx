a5218e28cd26f51d4e0a90d837ceec7fb91e60f4
Ritwik <ritwikhero@gmail.com>
2024-03-16 22:14:59 +0530

    almost done

---
+++ frontend/src/components/CreateToDo.jsx
@@ -0,0 +1,9 @@
+export function CreateToDo(){
+    return <div>
+        <input type="text" name="title" placeholder="title" id="" /><br />
+        <input type="text" name="dexcription" placeholder="description" id="" /><br />
+        <button>Add ToDo</button>
+
+
+    </div>
+}
